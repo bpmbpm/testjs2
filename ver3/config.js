12e455628583d66b583cc50dd2353a3a895e7698
Dmitry <121898072+bpmbpm@users.noreply.github.com>
2026-08-21 11:47:10 +0300

    Add files via upload

---
+++ ver3/config.js
@@ -0,0 +1,6 @@
+// config.js Ч только массив ссылок. Ћегко добавл€ть новые файлы.
+window.svgFileList = [
+    'https://bpmbpm.github.io/testjs2/ver1/svg/file1.svg',
+    'https://bpmbpm.github.io/testjs2/ver1/svg/file1-1.svg',
+    'https://bpmbpm.github.io/testjs2/ver1/svg/file1-2.svg'
+];
